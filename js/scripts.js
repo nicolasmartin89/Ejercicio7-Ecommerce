b3f0115272c765321d608bf028ce38620e99c8c4
Nicolas Martin <nicolasmartin89@gmail.com>
2024-07-28 11:59:37 -0300

    Resolve routes errors and script errors.

--- js/scripts.js
+++ js/scripts.js
@@ -1,120 +1,189 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const form = document.getElementById("personalForm");
-  const resultDiv = document.getElementById("result");
+  console.log("DOM completamente cargado y analizado");
 
   const htmlElement = document.documentElement;
   const switchElement = document.getElementById("darkModeSwitch");
+  const bodyElement = document.body;
 
-  const currentTheme = localStorage.getItem("bsTheme") || "dark";
-  htmlElement.setAttribute("data-bs-theme", currentTheme);
-  switchElement.checked = currentTheme === "dark";
-
-  switchElement.addEventListener("change", function () {
-    if (this.checked) {
-      htmlElement.setAttribute("data-bs-theme", "dark");
-      localStorage.setItem("bsTheme", "dark");
-    } else {
-      htmlElement.setAttribute("data-bs-theme", "light");
-      localStorage.setItem("bsTheme", "light");
-    }
-  });
-
-  form.addEventListener("submit", handleFormSubmit);
-
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    const formData = getFormData();
-    clearErrorMessages();
-
-    if (isFormValid(formData)) {
-      displayFormData(formData);
-    }
+  // Manejo de modo oscuro
+  if (switchElement) {
+      console.log("Switch de modo oscuro encontrado");
+      const currentTheme = localStorage.getItem("bsTheme") || "dark";
+      htmlElement.setAttribute("data-bs-theme", currentTheme);
+      bodyElement.classList.toggle("dark-mode", currentTheme === "dark");
+      switchElement.checked = currentTheme === "dark";
+  
+      switchElement.addEventListener("change", function () {
+          if (this.checked) {
+              htmlElement.setAttribute("data-bs-theme", "dark");
+              bodyElement.classList.add("dark-mode");
+              localStorage.setItem("bsTheme", "dark");
+          } else {
+              htmlElement.setAttribute("data-bs-theme", "light");
+              bodyElement.classList.remove("dark-mode");
+              localStorage.setItem("bsTheme", "light");
+          }
+      });
+  } else {
+      console.log("Switch de modo oscuro no encontrado");
   }
 
-  function getFormData() {
-    return {
-      nombre: document.getElementById("nombre").value.trim(),
-      apellido: document.getElementById("apellido").value.trim(),
-      email: document.getElementById("email").value.trim(),
-      consulta: document.getElementById("consulta").value.trim(),
-    };
+  // Búsqueda en tiempo real
+  const searchInput = document.getElementById('search');
+  if (searchInput) {
+      console.log("Campo de búsqueda encontrado");
+      searchInput.addEventListener('input', () => {
+          const filter = searchInput.value.toLowerCase();
+          const productCards = document.querySelectorAll('.card');
+          productCards.forEach(card => {
+              const title = card.querySelector('.card-title').innerText.toLowerCase();
+              if (title.includes(filter)) {
+                  card.parentElement.style.display = '';
+              } else {
+                  card.parentElement.style.display = 'none';
+              }
+          });
+      });
+  } else {
+      console.log("Campo de búsqueda no encontrado");
   }
 
-  function isFormValid(formData) {
-    let isValid = true;
+  // Mostrar la ventana emergente automáticamente después de 3 segundos
+  const offerModalElement = document.getElementById('offerModal');
+  if (offerModalElement) {
+      console.log("Modal de oferta encontrado");
+      setTimeout(function() {
+          var offerModal = new bootstrap.Modal(offerModalElement);
+          offerModal.show();
+          offerModalElement.querySelector('.modal-content').style.backgroundColor = '#ccc'; // Set modal background color
+      }, 3000);
+  } else {
+      console.log("Modal de oferta no encontrado");
+  }
 
-    if (!isNameValid(formData.nombre)) {
-      displayErrorForField(
-        "nombre-error",
-        "Complete el campo Nombre con la información correcta (solo letras, entre 2 y 60 caracteres)."
-      );
-      isValid = false;
-    }
+  // Cambio de color del botón
+  var botones = document.querySelectorAll('#miBoton');
+  if (botones.length > 0) {
+      console.log("Botón encontrado");
+      botones.forEach(boton => {
+          boton.addEventListener('click', function(event) {
+              event.preventDefault(); // Previene la navegación inmediata
+              this.classList.add('clicked');
+              
+              // Cambia el color de vuelta después de 300ms
+              setTimeout(() => {
+                  this.classList.remove('clicked');
+                  // Navega a la URL después de mostrar el efecto
+                  window.location.href = this.getAttribute('href');
+              }, 300);
+          });
+      });
+  } else {
+      console.log("Botón no encontrado");
+  }
 
-    if (!isNameValid(formData.apellido)) {
-      displayErrorForField(
-        "apellido-error",
-        "Complete el campo Apellido con la información correcta (solo letras, entre 2 y 60 caracteres)."
-      );
-      isValid = false;
-    }
+  // Manejo del formulario
+  const form = document.getElementById("personalForm");
+  const resultDiv = document.getElementById("result");
 
-    if (!isEmailValid(formData.email)) {
-      displayErrorForField(
-        "email-error",
-        "Complete el campo Email con una dirección válida (por ejemplo, usuario@dominio.com)."
-      );
-      isValid = false;
-    }
+  if (form) {
+      form.addEventListener("submit", handleFormSubmit);
 
-    if (!isConsultValid(formData.consulta)) {
-        displayErrorForField(
-          "consulta-error",
-          "Complete el campo Consulta con la información correcta (no puede estar vacío y solo puede contener letras y números)."
-        );
-        isValid = false;
+      function handleFormSubmit(event) {
+          event.preventDefault();
+          const formData = getFormData();
+          clearErrorMessages();
+
+          if (isFormValid(formData)) {
+              displayFormData(formData);
+          }
       }
 
-    return isValid;
-  }
+      function getFormData() {
+          return {
+              nombre: document.getElementById("nombre").value.trim(),
+              apellido: document.getElementById("apellido").value.trim(),
+              email: document.getElementById("email").value.trim(),
+              consulta: document.getElementById("consulta").value.trim(),
+          };
+      }
 
-  function isNameValid(name) {
-    const nameRegex = /^[a-zA-Z\s]{2,60}$/;
-    return nameRegex.test(name);
-  }
+      function isFormValid(formData) {
+          let isValid = true;
 
-  function isEmailValid(email) {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    return emailRegex.test(email);
-  }
+          if (!isNameValid(formData.nombre)) {
+              displayErrorForField(
+                  "nombre-error",
+                  "Complete el campo Nombre con la información correcta (solo letras, entre 2 y 60 caracteres)."
+              );
+              isValid = false;
+          }
 
-  function isConsultValid(consulta) {
-    const nameRegex = /^[a-zA-Z0-9]*$/;
-    return consulta.trim() !== "" && nameRegex.test(consulta);
-  }
+          if (!isNameValid(formData.apellido)) {
+              displayErrorForField(
+                  "apellido-error",
+                  "Complete el campo Apellido con la información correcta (solo letras, entre 2 y 60 caracteres)."
+              );
+              isValid = false;
+          }
 
-  function displayFormData(formData) {
-    alert(
-        `Formulario enviado con éxito:
-        
-            Nombre: ${formData.nombre}
-            Apellido: ${formData.apellido}
-            Email: ${formData.email}
-            Consulta: ${formData.consulta}`
-        
-    );
-    
-  }
+          if (!isEmailValid(formData.email)) {
+              displayErrorForField(
+                  "email-error",
+                  "Complete el campo Email con una dirección válida (por ejemplo, usuario@dominio.com)."
+              );
+              isValid = false;
+          }
 
-  function displayErrorForField(fieldId, message) {
-    const errorSpan = document.getElementById(fieldId);
-    errorSpan.textContent = message;
-  }
+          if (!isConsultValid(formData.consulta)) {
+              displayErrorForField(
+                  "consulta-error",
+                  "Complete el campo Consulta con la información correcta (no puede estar vacío y solo puede contener letras y números)."
+              );
+              isValid = false;
+          }
 
-  function clearErrorMessages() {
-    const errorSpans = document.querySelectorAll(".error-message");
-    errorSpans.forEach((span) => {
-      span.textContent = "";
-    });
+          return isValid;
+      }
+
+      function isNameValid(name) {
+          const nameRegex = /^[a-zA-Z\s]{2,60}$/;
+          return nameRegex.test(name);
+      }
+
+      function isEmailValid(email) {
+          const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+          return emailRegex.test(email);
+      }
+
+      function isConsultValid(consulta) {
+          const nameRegex = /^[a-zA-Z0-9]*$/;
+          return consulta.trim() !== "" && nameRegex.test(consulta);
+      }
+
+      function displayFormData(formData) {
+          alert(
+              `Formulario enviado con éxito:
+              
+              Nombre: ${formData.nombre}
+              Apellido: ${formData.apellido}
+              Email: ${formData.email}
+              Consulta: ${formData.consulta}`
+          );
+      }
+
+      function displayErrorForField(fieldId, message) {
+          const errorSpan = document.getElementById(fieldId);
+          errorSpan.textContent = message;
+      }
+
+      function clearErrorMessages() {
+          const errorSpans = document.querySelectorAll(".error-message");
+          errorSpans.forEach((span) => {
+              span.textContent = "";
+          });
+      }
+  } else {
+      console.log("Formulario personal no encontrado");
   }
 });
